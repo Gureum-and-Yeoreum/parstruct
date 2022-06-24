4154a433eb89a2d759e9233e9e4555cb52645cc0
Gureum-and-Yeoreum <yeoreumcloud@gmail.com>
2022-06-24 13:29:14 +0900

    now working?

--- parstruct.m
+++ parstruct.m
@@ -1,18 +1,23 @@
 function [structed] = parstruct(structed,nvars,varname,n,varargin)
 
+n_field = size(varname,2);
+
 if size(varname,1) ~= 1
     varname = varname.';
 end
 
-n_field = size(varname,2);
+if size(varargin,2) ~= nvars
+    error("nvars and varargin size does not same")
+end
 
 if nvars ~= n_field
-    error("Can not match variables and fields")
+    error("nvars and varname size does not same")
 end
 
 if n == 1
-    varargin = varargin.';
-    structed = cell2struct(varargin,varname,1);
+    for i = 1:nvars
+        structed.(varname(i)) = cell2mat(varargin(i));
+    end
 else
     for i = 1:nvars
         structed.(varname(i)) = cat(ndims(varargin(i))+1,structed.(varname(i)),cell2mat(varargin(i)));
